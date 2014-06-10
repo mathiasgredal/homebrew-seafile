6f24997c0ca6b94ca085013ffd6e1818ad7a66ca
Chilledheart <rwindz0@gmail.com>
2014-06-10 22:19:26 +0800

    Add ccnet formula

--- README.md
+++ README.md
@@ -3,7 +3,9 @@ Usage
 
 1. ``brew tap Chilledheart/seafile``
 1. ``brew install libsearpc``
+1. ``brew install ccnet``
 
-If you need install HEAD version, try
+If the stable build broken, please try HEAD build
 
 1. ``brew instlal --HEAD libsearpc``
+1. ``brew instlal --HEAD ccnet``
---
+++ ccnet.rb
@@ -0,0 +1,44 @@
+require "formula"
+
+class Ccnet < Formula
+  homepage "http://www.seafile.com/"
+  stable do
+    url "https://github.com/haiwen/ccnet/archive/v3.0.5-pro.tar.gz"
+    version "3.0.5-pro"
+    sha1 "9aa407a3d41a3340110f41c99f39ff9146b2ceaa"
+  end
+  head do
+    url "https://github.com/haiwen/ccnet.git"
+  end
+
+  #Silence the annoying "subdir-objects disabled" warning
+  patch :p1 do
+    url "https://github.com/Chilledheart/ccnet/commit/4e290a5c.diff"
+    sha1 "f97591c4ef2d5baae5a4b2a10bdc31f6967face9"
+  end
+  #[FIX] homebrew autotools path
+  patch :p1 do
+    url "https://github.com/Chilledheart/ccnet/commit/48610f08.diff"
+    sha1 "af5a53ffb9fca0d4918b9d2ef1afd54959ecf186"
+  end
+
+  depends_on 'autoconf' => :build
+  depends_on "automake" => :build
+  depends_on 'pkg-config' => :build
+  depends_on 'libtool' => :build
+  depends_on 'intltool' => :build
+  depends_on 'glib'
+  depends_on 'ossp-uuid'
+  depends_on 'libsearpc'
+  depends_on 'jansson'
+  depends_on 'libzdb'
+  depends_on 'vala'
+
+  def install
+    system "./autogen.sh"
+    system "./configure", "--prefix=#{prefix}", "--disable-server", "--enable-client"
+    system "python `which searpc-codegen.py` ./lib/rpc_table.py"
+    system "make"
+    system "make", "install"
+  end
+end
