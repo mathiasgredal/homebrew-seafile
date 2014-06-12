80f2ad1a2ed96bec80990e07f43182dedb2f500a
Chilledheart <rwindz0@gmail.com>
2014-06-12 14:16:35 +0800

    libevhtp: add brewed-openssl support

--- libevhtp.rb
+++ libevhtp.rb
@@ -2,16 +2,16 @@ require "formula"
 
 class Libevhtp < Formula
   homepage "https://github.com/ellzey/libevhtp"
-  stable do
-    url "https://github.com/ellzey/libevhtp/archive/1.2.9.tar.gz"
-    sha1 "c4edfcfd1427db26e585ee7bc9bce7df4f10ec3d"
-  end
-  head do
-    url "https://github.com/ellzey/libevhtp.git"
-  end
+  url "https://github.com/ellzey/libevhtp/archive/1.2.9.tar.gz"
+  sha1 "c4edfcfd1427db26e585ee7bc9bce7df4f10ec3d"
+
+  head "https://github.com/ellzey/libevhtp.git"
+
+  option 'with-brewed-openssl', "Build with Homebrew OpenSSL"
 
   depends_on 'cmake' => :build
   depends_on 'libevent'
+  depends_on 'openssl' if build.with? 'brewed-openssl'
 
   def install
     system "cmake", ".", *std_cmake_args
