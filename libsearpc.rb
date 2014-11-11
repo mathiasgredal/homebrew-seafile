82d124e7dbcffdbce7823cca4121f0c8c023c86f
Chilledheart <rwindz0@gmail.com>
2014-11-11 17:30:24 +0800

    remove included upsteam patch

--- libsearpc.rb
+++ libsearpc.rb
@@ -8,20 +8,10 @@ class Libsearpc < Formula
 
   head "https://github.com/haiwen/libsearpc.git"
 
-  #Detect darwin platform for autotools
-  patch :p1 do
-    url "https://github.com/Chilledheart/libsearpc/commit/2e2adb.diff"
-    sha1 "3512f7a105d9f89be9389aba71996d08601e3cfb"
-  end
   #FIX for homebrew autotools path
   patch :p1 do
     url "https://github.com/Chilledheart/libsearpc/commit/70d44f.diff"
     sha1 "ac4c411d9793ba88f1cd66975c19635d3fab36ef"
-  end
-  #Fix a bug with coreutils installed under Mac OS X
-  patch :p1 do
-    url "https://github.com/Chilledheart/libsearpc/commit/665832.diff"
-    sha1 "b33f4e571c3ad77593d18130a077fbf40f14358d"
   end
 
   depends_on 'autoconf' => :build
